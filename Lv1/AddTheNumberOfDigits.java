ce364163bd66fa6f4ed3e3858e2541f4d4890f07
hongseungkeun <eiziong1@gmail.com>
2023-07-14 20:31:03 +0900

    프로그래머스 : 자릿수 더하기

---
+++ Lv1/AddTheNumberOfDigits.java
@@ -0,0 +1,25 @@
+package Lv1;
+
+import java.util.Arrays;
+
+/**
+ *  프로그래머스 : 자릿수 더하기
+ *
+ *  문제 설명
+ *
+ *  자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+ *  예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+ */
+
+public class AddTheNumberOfDigits {
+    public int solution(int n) {
+        String[] arr = String.valueOf(n).split("");
+        return Arrays.stream(arr).mapToInt(Integer::parseInt).sum();
+    }
+
+    public static void main(String[] args) {
+        int n = 123;
+        AddTheNumberOfDigits addTheNumberOfDigits = new AddTheNumberOfDigits();
+        System.out.println(addTheNumberOfDigits.solution(n));
+    }
+}
