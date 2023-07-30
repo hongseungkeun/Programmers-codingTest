ab12ffa2752e8bed605cbf29aea5d4b1c6aed462
hongseungkeun <eiziong1@gmail.com>
2023-07-30 23:34:52 +0900

    프로그래머스 : 숫자 짝꿍

--- Lv1/NumberPartner.java
+++ Lv1/NumberPartner.java
@@ -29,7 +29,7 @@ import java.util.Map;
 
 public class NumberPartner {
     public String solution(String X, String Y) {
-        String answer = "";
+        String answer;
         StringBuilder sb = new StringBuilder();
         Map<Character, Integer> yMap = new HashMap<>();
 
