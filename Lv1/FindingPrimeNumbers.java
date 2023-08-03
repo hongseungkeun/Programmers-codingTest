91abd35f811d3c7ecab95fbc69486ef6e11a06f2
hongseungkeun <eiziong1@gmail.com>
2023-08-03 23:33:25 +0900

    프로그래머스 : 소수찾기

---
+++ Lv1/FindingPrimeNumbers.java
@@ -0,0 +1,57 @@
+package Lv1;
+
+/**
+ *  프로그래머스 : 소수찾기
+ *
+ *  문제 설명
+ *
+ *  1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+ *  소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+ *  (1은 소수가 아닙니다.)
+ *
+ *  제한 조건
+ *  - n은 2이상 1000000이하의 자연수입니다.
+ *
+ *  풀이 과정
+ *  - n + 1개의 수만큼 가진 int[] 배열 arr안에 모든 수 대입
+ *  - arr[0], arr[1]은 각각 0, 1로 소수가 아니므로 0 대입
+ *  - 모든 수의 2의 배수 값들을 찾아 0으로 대입
+ *  - arr에서 0이 아닌 것만큼 answer++
+ */
+
+public class FindingPrimeNumbers {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        arr[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if(arr[i] == 0) {
+                continue;
+            }
+
+            for (int j = i * 2; j <= n; j+=i) {
+                arr[j] = 0;
+            }
+        }
+
+        for (int a : arr) {
+            if (a != 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        FindingPrimeNumbers findingPrimeNumbers = new FindingPrimeNumbers();
+        System.out.println(findingPrimeNumbers.solution(n));
+    }
+}
