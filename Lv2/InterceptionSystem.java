5b0c46ccf9105bb63106ac857704d26c9cf223b6
hongseungkeun <eiziong1@gmail.com>
2023-08-04 22:03:15 +0900

    프로그래머스 : 요격 시스템

--- Lv2/InterceptionSystem.java
+++ Lv2/InterceptionSystem.java
@@ -27,7 +27,7 @@ public class InterceptionSystem {
     public int solution(int[][] targets) {
         int answer = 0;
 
-        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
+        Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));
 
         int end = targets[0][1];
         answer++;
