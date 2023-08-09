fd1aa94c0daeb21b86543b4004274318741ec593
hongseungkeun <eiziong1@gmail.com>
2023-08-10 00:07:56 +0900

    프로그래머스 : 전화번호 목록

--- Lv2/Phone.java
+++ Lv2/Phone.java
@@ -4,15 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 프로그래머스 : 전화번호 목록
+ *  프로그래머스 : 전화번호 목록
  *
- * - 전화번호부에 적힌 전화번호에 다른 전화번호의 접두사인 경우 false, 아니면 true
+ *  문제 설명
  *
- * 풀이
- * - 전화번호부들을 Map의 키값으로 넣음
- * - 전화번호부들을 반복문으로 돌려서 찾기
- * - 전화번호부를 substring으로 자른 값들을 Map의 containsKey 메소드를 통해 있다면 false 리턴
+ *  전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때,
+ *  어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+ *
+ *  풀이 과정
+ *  - 전화번호부들을 Map의 키값으로 넣음
+ *  - 전화번호부들을 반복문으로 돌려서 찾기
+ *  - 전화번호부를 substring으로 자른 값들을 Map의 containsKey 메소드를 통해 있다면 false 리턴
  * */
+
 public class Phone {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
@@ -23,7 +27,6 @@ public class Phone {
         for (String phone : phone_book) {
             for (int i = 0; i < phone.length(); i++) {
                 if(phoneList.containsKey(phone.substring(0,i))){
-                    phone.substring(0,i);
                     return false;
                 }
             }
